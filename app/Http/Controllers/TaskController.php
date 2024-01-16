7029f9c4f8997048a9277c4f111e25cb6342e173
Brenda Mollaret <mollaretbrenda@gmail.com>
2024-01-16 20:23:24 -0300

    Actualizar creado. Botón Nueva tarea y editar funcionan.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\task;
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\RedirectResponse; //Laravel
+use illuminate\View\View; //Laravel
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View //:View donde sea que devolvamos una vista hay que ponerlo, es de laravel.
     {
         $tasks = Task::latest()->paginate(3); //las trae de la base de datos y las guarda acá(get) //Luego puse paginate para que nos muestre 3 tareas nomas
         return view('index',['tasks'=>$tasks]);
@@ -20,7 +21,7 @@ class TaskController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View 
     {
         return view('create');
     }
@@ -28,7 +29,7 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse //esto es de laravel RedirectReponse
+    public function store(Request $request): RedirectResponse //esto es de laravel RedirectReponse, cada vez que redirijamos debemos ponerlo.
     {
         //validación
         $request->validate([
@@ -52,17 +53,25 @@ class TaskController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(task $task)
-    {
-        //
+    public function edit(task $task): View
+    {   
+        return view('edit',['task'=> $task]);
+        //dd($task);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, task $task)
+    public function update(Request $request, task $task): RedirectResponse 
     {
-        //
+        //validación
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        $task->update($request->all());
+        return redirect()->route('tasks.index')->with('success','Tarea actualizada correctamente');
+        //dd($request->all());
     }
 
     /**
