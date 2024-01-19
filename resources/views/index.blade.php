354090531bd218f1bb80282045cee34c0e404000
Brenda Mollaret <mollaretbrenda@gmail.com>
2024-01-19 13:47:01 -0300

    cambio de colores a los estados

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-12">
         <div>
-            <h2 class="text-white">CRUD de Tareas</h2>
+            <h2 class="text-white mt-3">CRUD de Tareas</h2>
         </div>
         <div>
             <a href="{{route('tasks.create')}}" class="btn btn-primary">Crear tarea</a>
@@ -34,14 +34,23 @@
                      {{$task->due_date}}
                  </td>
                  <td>
-                     <span class="badge bg-warning fs-6">{{$task->status}}</span>
-                 </td>
+                    @if($task->status == 'Pendiente')
+                        <span class="badge bg-warning fs-6">{{$task->status}}</span>
+                    @elseif($task->status == 'En progreso')
+                        <span class="badge bg-secondary fs-6">{{$task->status}}</span>
+                    @elseif($task->status == 'Completada')
+                        <span class="badge bg-success fs-6">{{$task->status}}</span>
+                    @else
+                        <span class="badge bg-secondary fs-6">{{$task->status}}</span>
+                    @endif
+                </td>
+                
                  <td>
                      <a href="{{route('tasks.edit', $task)}}" class="btn btn-warning">Editar</a>
             
                      <form action="{{route('tasks.destroy', $task)}}" method="POST" class="d-inline">
                         @csrf
-                        @method('DELETE')
+                        @method('DELETE') <!-- Por lo que no recibe metodos de API-->
                          <button type="submit" class="btn btn-danger">Eliminar</button>
                      </form>
                  </td>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body class="bg-dark text-white">
     <div class="container">
-        @yield('content') 
+        @yield('content') <!-- Acá va el contenido dinámico / Intenta semejar React -->
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
